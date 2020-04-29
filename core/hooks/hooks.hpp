fcfe2ac08a69bb58f4ca770b798a656338da30a5
gabrielvulk <51248378+cristeigabriel@users.noreply.github.com>
2020-04-29 18:54:44 +0300

    Make your hooks static.

--- core/hooks/hooks.hpp
+++ core/hooks/hooks.hpp
@@ -6,12 +6,12 @@ namespace hooks {
 
 	namespace create_move {
 		using fn = bool(__stdcall*)(float, c_usercmd*);
-		bool __fastcall hook(void* ecx, void* edx, int input_sample_frametime, c_usercmd* cmd);
+		static bool __fastcall hook(void* ecx, void* edx, int input_sample_frametime, c_usercmd* cmd);
 	};
 
 	namespace paint_traverse {
 		using fn = void(__thiscall*)(i_panel*, unsigned int, bool, bool);
-		void __stdcall hook(unsigned int panel, bool force_repaint, bool allow_force);
+		static void __stdcall hook(unsigned int panel, bool force_repaint, bool allow_force);
 	}
 
 }
