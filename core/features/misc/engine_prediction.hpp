e02ff5cd60e806ec509aef4634a8af853d07de2a
gabrielvulk <51248378+cristeigabriel@users.noreply.github.com>
2020-04-29 18:58:12 +0300

    Tidy up engine prediction

--- core/features/misc/engine_prediction.hpp
+++ core/features/misc/engine_prediction.hpp
@@ -1,7 +1,12 @@
 #pragma once
-#include "../../../dependencies/common_includes.hpp"
+#include "../../../dependencies/utilities/csgo.hpp"
 
 namespace prediction {
 	void start(c_usercmd* cmd);
 	void end();
+
+	inline player_move_data data;
+	inline float old_cur_time;
+	inline float old_frame_time;
+	inline int* prediction_random_seed;
 };
