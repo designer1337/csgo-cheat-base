4a9e9d5f5797f764a8699a4480735c4029ebad0f
Marius <53657322+bruhmoment21@users.noreply.github.com>
2020-07-01 09:34:57 +0300

    Update i_localize.hpp

--- dependencies/interfaces/i_localize.hpp
+++ dependencies/interfaces/i_localize.hpp
@@ -4,6 +4,6 @@ class i_localize {
 public:
 	wchar_t* find(const char* token_name) {
 		using original_fn = wchar_t* (__thiscall*)(i_localize*, const char*);
-		return (*(original_fn * *)this)[11](this, token_name);
+		return (*(original_fn * *)this)[12](this, token_name);
 	}
 };
