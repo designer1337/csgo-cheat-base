0218a055e67ee733afb909eeb332248eb133a9f8
sove <13748681+behality@users.noreply.github.com>
2020-07-03 23:13:19 +0300

    CamelCase to snake_case

--- dependencies/interfaces/i_app_system.hpp
+++ dependencies/interfaces/i_app_system.hpp
@@ -1,21 +1,18 @@
 #pragma once
 
-/* declarations */
 class i_app_system;
 
-/* defs */
-typedef void* (*create_interface_fn)(const char* pName, int* pReturnCode);
+typedef void* (*create_interface_fn)(const char* name, int* return_code);
 
-/* functions */
 class i_app_system {
 public:
 	virtual bool connect(create_interface_fn factory) = 0;
 	virtual void disconnect() = 0;
-	virtual void* query_interface(const char* pInterfaceName) = 0;
+	virtual void* query_interface(const char* interface_name) = 0;
 	virtual int init() = 0;
 	virtual void shutdown() = 0;
 	virtual const void* get_dependencies() = 0;
 	virtual int  get_tier() = 0;
-	virtual void reconnect(create_interface_fn factory, const char* pInterfaceName) = 0;
+	virtual void reconnect(create_interface_fn factory, const char* interface_name) = 0;
 	virtual void unknown() = 0;
 };
