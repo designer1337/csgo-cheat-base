82150e43678d9d93afd74fd3a341d87c5c739618
sove <13748681+behality@users.noreply.github.com>
2020-07-03 22:28:24 +0300

    Update models.hpp

--- source-sdk/structs/models.hpp
+++ source-sdk/structs/models.hpp
@@ -5,10 +5,10 @@ struct model_t {
 
 	void	*handle;
 	char	name[260];
-	int		load_flags;
-	int		server_count;
-	int		type;
-	int		flags;
+	int	load_flags;
+	int	server_count;
+	int	type;
+	int	flags;
 	vec3_t	vec_mins;
 	vec3_t	vec_maxs;
 	float	radius;
@@ -16,20 +16,19 @@ struct model_t {
 
 struct model_render_info_t {
 
-	vec3_t			origin;
-	vec3_t			angles;
-	char			u0[0x4];
-	void			*renderable;
-	//const void		*model;
+	vec3_t		origin;
+	vec3_t		angles;
+	char		u0[0x4];
+	void		*renderable;
 	const model_t	*model;
 	const matrix_t	*model_to_world;
 	const matrix_t	*lighting_offset;
 	const vec3_t	*lighting_origin;
-	int				flags;
-	int				entity_index;
-	int				skin;
-	int				body;
-	int				hitboxset;
+	int		flags;
+	int		entity_index;
+	int		skin;
+	int		body;
+	int		hitboxset;
 	unsigned short	instance;
 
 	model_render_info_t() {
