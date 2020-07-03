4365c542561a1a48e5dbfd7b49cba883db1752ab
sove <13748681+behality@users.noreply.github.com>
2020-07-03 23:49:15 +0300

    Update i_material_system.hpp

--- dependencies/interfaces/i_material_system.hpp
+++ dependencies/interfaces/i_material_system.hpp
@@ -4,34 +4,34 @@
 #define MAXSTUDIOSKINS		32
 
 // These are given to FindMaterial to reference the texture groups that Show up on the
-#define TEXTURE_GROUP_LIGHTMAP						        "Lightmaps"
-#define TEXTURE_GROUP_WORLD							          "World textures"
-#define TEXTURE_GROUP_MODEL							          "Model textures"
-#define TEXTURE_GROUP_VGUI							          "VGUI textures"
-#define TEXTURE_GROUP_PARTICLE						        "Particle textures"
-#define TEXTURE_GROUP_DECAL							          "Decal textures"
-#define TEXTURE_GROUP_SKYBOX						          "SkyBox textures"
-#define TEXTURE_GROUP_CLIENT_EFFECTS				      "ClientEffect textures"
-#define TEXTURE_GROUP_OTHER							          "Other textures"
-#define TEXTURE_GROUP_PRECACHED						        "Precached"
-#define TEXTURE_GROUP_CUBE_MAP						        "CubeMap textures"
-#define TEXTURE_GROUP_RENDER_TARGET					      "RenderTargets"
-#define TEXTURE_GROUP_UNACCOUNTED					        "Unaccounted textures"
-//#define TEXTURE_GROUP_STATIC_VERTEX_BUFFER		  "Static Vertex"
-#define TEXTURE_GROUP_STATIC_INDEX_BUFFER			    "Static Indices"
+#define TEXTURE_GROUP_LIGHTMAP				"Lightmaps"
+#define TEXTURE_GROUP_WORLD				"World textures"
+#define TEXTURE_GROUP_MODEL				"Model textures"
+#define TEXTURE_GROUP_VGUI				"VGUI textures"
+#define TEXTURE_GROUP_PARTICLE				"Particle textures"
+#define TEXTURE_GROUP_DECAL				"Decal textures"
+#define TEXTURE_GROUP_SKYBOX				"SkyBox textures"
+#define TEXTURE_GROUP_CLIENT_EFFECTS			"ClientEffect textures"
+#define TEXTURE_GROUP_OTHER				"Other textures"
+#define TEXTURE_GROUP_PRECACHED				"Precached"
+#define TEXTURE_GROUP_CUBE_MAP				"CubeMap textures"
+#define TEXTURE_GROUP_RENDER_TARGET			"RenderTargets"
+#define TEXTURE_GROUP_UNACCOUNTED			"Unaccounted textures"
+//#define TEXTURE_GROUP_STATIC_VERTEX_BUFFER		"Static Vertex"
+#define TEXTURE_GROUP_STATIC_INDEX_BUFFER		"Static Indices"
 #define TEXTURE_GROUP_STATIC_VERTEX_BUFFER_DISP		"Displacement Verts"
 #define TEXTURE_GROUP_STATIC_VERTEX_BUFFER_COLOR	"Lighting Verts"
 #define TEXTURE_GROUP_STATIC_VERTEX_BUFFER_WORLD	"World Verts"
 #define TEXTURE_GROUP_STATIC_VERTEX_BUFFER_MODELS	"Model Verts"
 #define TEXTURE_GROUP_STATIC_VERTEX_BUFFER_OTHER	"Other Verts"
-#define TEXTURE_GROUP_DYNAMIC_INDEX_BUFFER			  "Dynamic Indices"
-#define TEXTURE_GROUP_DYNAMIC_VERTEX_BUFFER			  "Dynamic Verts"
-#define TEXTURE_GROUP_DEPTH_BUFFER					      "DepthBuffer"
-#define TEXTURE_GROUP_VIEW_MODEL					        "ViewModel"
-#define TEXTURE_GROUP_PIXEL_SHADERS					      "Pixel Shaders"
-#define TEXTURE_GROUP_VERTEX_SHADERS				      "Vertex Shaders"
-#define TEXTURE_GROUP_RENDER_TARGET_SURFACE			  "RenderTarget Surfaces"
-#define TEXTURE_GROUP_MORPH_TARGETS					      "Morph Targets"
+#define TEXTURE_GROUP_DYNAMIC_INDEX_BUFFER		"Dynamic Indices"
+#define TEXTURE_GROUP_DYNAMIC_VERTEX_BUFFER		"Dynamic Verts"
+#define TEXTURE_GROUP_DEPTH_BUFFER			"DepthBuffer"
+#define TEXTURE_GROUP_VIEW_MODEL			"ViewModel"
+#define TEXTURE_GROUP_PIXEL_SHADERS			"Pixel Shaders"
+#define TEXTURE_GROUP_VERTEX_SHADERS			"Vertex Shaders"
+#define TEXTURE_GROUP_RENDER_TARGET_SURFACE		"RenderTarget Surfaces"
+#define TEXTURE_GROUP_MORPH_TARGETS			"Morph Targets"
 
 class	i_ms_mat;
 class	i_ms_mesh;
@@ -204,7 +204,6 @@ typedef unsigned short mat_handle_t;
 DECLARE_POINTER_HANDLE( mat_lock_t );
 
 class i_material_system {
-
 public:
 	i_material *find_material( char const *material_name, const char *group_name, bool complain = true, const char *complain_prefix = 0 ) {
 		using fn = i_material * ( __thiscall * )( i_material_system *, char const *, const char *, bool, const char * );
