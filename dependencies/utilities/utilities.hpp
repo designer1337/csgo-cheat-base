84b5c13609ddb47f7c160eb7f9bcbc591edf5796
sove <13748681+behality@users.noreply.github.com>
2020-07-03 22:51:51 +0300

    CamelCase to snake_case

--- dependencies/utilities/utilities.hpp
+++ dependencies/utilities/utilities.hpp
@@ -3,11 +3,11 @@
 #include "../../source-sdk/math/vector3d.hpp"
 
 namespace utilities {
-	template<typename FuncType>
-	__forceinline static FuncType call_virtual(void* ppClass, int index) {
-		int* pVTable = *(int**)ppClass;
-		int dwAddress = pVTable[index];
-		return (FuncType)(dwAddress);
+	template< typename t >
+	__forceinline static t call_virtual( void *name, int offset ) {
+		int *table = *( int ** ) name;
+		int address = table[ offset ];
+		return ( t ) ( address );
 	}
 
 	std::uint8_t* pattern_scan(const char* module_name, const char* signature) noexcept;
