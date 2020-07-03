79258186b48d4cfdf9da75732e650e83a8122060
sove <13748681+behality@users.noreply.github.com>
2020-07-03 23:27:43 +0300

    Fixed utf8 characters rendering

--- dependencies/utilities/renderer/renderer.cpp
+++ dependencies/utilities/renderer/renderer.cpp
@@ -16,7 +16,6 @@ void render::draw_line(std::int32_t x1, std::int32_t y1, std::int32_t x2, std::i
 }
 
 void render::text(std::int32_t x, std::int32_t y, unsigned long font, const wchar_t *text, bool centered, color color) {
-
 	interfaces::surface->draw_text_font(font);
 	int text_width, text_height;
 
@@ -32,13 +31,12 @@ void render::text(std::int32_t x, std::int32_t y, unsigned long font, const wcha
 }
 
 void render::text(std::int32_t x, std::int32_t y, unsigned long font, std::string text, bool centered, color color) {
-
 	wchar_t temp[128];
 	int text_width, text_height;
 	if (MultiByteToWideChar(CP_UTF8, 0, text.c_str(), -1, temp, 128) > 0) {
 		interfaces::surface->draw_text_font(font);
 		if (centered) {
-			interfaces::surface->get_text_size(font, text, text_width, text_height);
+			interfaces::surface->get_text_size(font, temp, text_width, text_height);
 			interfaces::surface->draw_text_pos(x - text_width / 2, y);
 		}
 		else
@@ -48,22 +46,22 @@ void render::text(std::int32_t x, std::int32_t y, unsigned long font, std::strin
 	}
 }
 
-void render::draw_rect(std::int32_t x, std::int32_t y, std::int32_t w, std::int32_t h, color color) {
+void render::draw_rect(std::int32_t x, std::int32_t y, std::int32_t width, std::int32_t height, color color) {
 	interfaces::surface->set_drawing_color(color.r, color.g, color.b, color.a);
-	interfaces::surface->draw_outlined_rect(x, y, w, h);
+	interfaces::surface->draw_outlined_rect(x, y, width, height);
 }
 
-void render::draw_filled_rect(std::int32_t x, std::int32_t y, std::int32_t w, std::int32_t h, color color) {
+void render::draw_filled_rect(std::int32_t x, std::int32_t y, std::int32_t width, std::int32_t height, color color) {
 	interfaces::surface->set_drawing_color(color.r, color.g, color.b, color.a);
-	interfaces::surface->draw_filled_rectangle(x, y, w, h);
+	interfaces::surface->draw_filled_rectangle(x, y, width, height);
 }
 
-void render::draw_outline(std::int32_t x, std::int32_t y, std::int32_t w, std::int32_t h, color color) {
+void render::draw_outline(std::int32_t x, std::int32_t y, std::int32_t width, std::int32_t height, color color) {
 	interfaces::surface->set_drawing_color(color.r, color.g, color.b, color.a);
-	interfaces::surface->draw_outlined_rect(x, y, w, h);
+	interfaces::surface->draw_outlined_rect(x, y, width, height);
 }
 
-void render::draw_textured_polygon(std::int32_t n, vertex_t* vertice, color color) {
+void render::draw_textured_polygon(std::int32_t vertices_count, vertex_t* vertices, color color) {
 	static unsigned char buf[4] = { 255, 255, 255, 255 };
 	unsigned int texture_id{};
 	if (!texture_id) {
@@ -72,16 +70,16 @@ void render::draw_textured_polygon(std::int32_t n, vertex_t* vertice, color colo
 	}
 	interfaces::surface->set_drawing_color(color.r, color.g, color.b, color.a);
 	interfaces::surface->set_texture(texture_id);
-	interfaces::surface->draw_polygon(n, vertice);
+	interfaces::surface->draw_polygon(vertices_count, vertices);
 }
 
-void render::draw_circle(std::int32_t x, std::int32_t y, std::int32_t r, std::int32_t s, color color) {
-	float step = M_PI * 2.0 / s;
+void render::draw_circle(std::int32_t x, std::int32_t y, std::int32_t radius, std::int32_t segments, color color) {
+	float step = M_PI * 2.0 / segments;
 	for (float a = 0; a < (M_PI * 2.0); a += step) {
-		float x1 = r * cos(a) + x;
-		float y1 = r * sin(a) + y;
-		float x2 = r * cos(a + step) + x;
-		float y2 = r * sin(a + step) + y;
+		float x1 = radius * cos(a) + x;
+		float y1 = radius * sin(a) + y;
+		float x2 = radius * cos(a + step) + x;
+		float y2 = radius * sin(a + step) + y;
 		interfaces::surface->set_drawing_color(color.r, color.g, color.b, color.a);
 		interfaces::surface->draw_line(x1, y1, x2, y2);
 	}
