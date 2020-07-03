9fe0d637f711f23ebc0eb16277f8977fc4f99f74
sove <13748681+behality@users.noreply.github.com>
2020-07-03 23:08:58 +0300

    CamelCase to snake_case

--- dependencies/interfaces/i_input.hpp
+++ dependencies/interfaces/i_input.hpp
@@ -2,15 +2,18 @@
 #include "../../source-sdk/classes/c_usercmd.hpp"
 
 class i_input {
+private:
+	char u0[ 172 ];
+	bool u1;
 public:
-	char pad_0000[172]; //0x0000
-	bool N0000004E; //0x00AC
-	bool m_fCameraInThirdPerson; //0x00AD
-	char pad_00AE[2]; //0x00AE
-	vec3_t m_vecCameraOffset; //0x00B0
+	bool camera_in_third_person;
+private:
+	char u2[ 2 ];
+public:
+	vec3 camera_offset;
 
-	c_usercmd* get_user_cmd(int slot, int sequence_num) {
-		using original_fn = c_usercmd * (__thiscall*)(void*, int, int);
-		return (*(original_fn * *)this)[8](this, slot, sequence_num);
+	c_usercmd *get_user_cmd( int slot, int sequence_num ) {
+		using fn = c_usercmd * ( __thiscall * )( void *, int, int );
+		return ( *( fn ** ) this )[ 8 ]( this, slot, sequence_num );
 	}
 };
