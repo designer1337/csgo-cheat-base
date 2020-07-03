d4fa8145525f5f9a02159c29b50146f2ec522de0
sove <13748681+behality@users.noreply.github.com>
2020-07-03 23:06:26 +0300

    CamelCase to snake_case

--- dependencies/interfaces/i_player_movement.hpp
+++ dependencies/interfaces/i_player_movement.hpp
@@ -3,83 +3,92 @@
 #include "../../source-sdk/classes/c_usercmd.hpp"
 
 class player_move_helper {
+
 public:
+	bool	first_run_of_iunctions : 1;
+	bool	game_code_moved_player : 1;
+	int	player_handle;
+	int	impulse_command;
+	vec3	view_angles;
+	vec3	abs_view_angles;
+	int	buttons;
+	int	old_buttons;
+	float	forward_move;
+	float	side_move;
+	float	up_move;
+	float	max_speed;
+	float	client_max_speed;
+	vec3	velocity;
+	vec3	angles;
+	vec3	old_angles;
+	float	out_step_height;
+	vec3	wish_velocity;
+	vec3	jump_velocity;
+	vec3	constraint_center;
+	float	constraint_radius;
+	float	constraint_width;
+	float	constraint_speed_factor;
 
-	bool bFirstRunOfFunctions : 1;
-	bool bGameCodeMovedPlayer : 1;
-	int nPlayerHandle; // edict index on server, client entity handle on client=
-	int nImpulseCommand; // Impulse command issued.
-	vec3_t vecViewAngles; // Command view angles (local space)
-	vec3_t vecAbsViewAngles; // Command view angles (world space)
-	int nButtons; // Attack buttons.
-	int nOldButtons; // From host_client->oldbuttons;
-	float flForwardMove;
-	float flSideMove;
-	float flUpMove;
-	float flMaxSpeed;
-	float flClientMaxSpeed;
-	vec3_t vecVelocity; // edict::velocity // Current movement direction.
-	vec3_t vecAngles; // edict::angles
-	vec3_t vecOldAngles;
-	float outStepHeight; // how much you climbed this move
-	vec3_t outWishVel; // This is where you tried
-	vec3_t outJumpVel; // This is your jump velocity
-	vec3_t vecConstraintCenter;
-	float flConstraintRadius;
-	float flConstraintWidth;
-	float flConstraintSpeedFactor;
-	float flUnknown[5];
-	vec3_t vecAbsOrigin;
+private:
+	float	u0[5];
 
-	virtual	void _vpad() = 0;
-	virtual void set_host(player_t* host) = 0;
+public:
+	vec3	abs_origin;
+
+	virtual	void u1() = 0;
+	virtual void set_host(player_t *host) = 0;
 };
 
 class player_move_data {
+
 public:
-	bool    bFirstRunOfFunctions : 1;
-	bool    bGameCodeMovedPlayer : 1;
-	int     nPlayerHandle;        // edict index on server, client entity handle on client=
-	int     nImpulseCommand;      // Impulse command issued.
-	vec3_t    vecViewAngles;        // Command view angles (local space)
-	vec3_t  vecAbsViewAngles;     // Command view angles (world space)
-	int     nButtons;             // Attack buttons.
-	int     nOldButtons;          // From host_client->oldbuttons;
-	float   flForwardMove;
-	float   flSideMove;
-	float   flUpMove;
-	float   flMaxSpeed;
-	float   flClientMaxSpeed;
-	vec3_t  vecVelocity;          // edict::velocity        // Current movement direction.
-	vec3_t  vecAngles;            // edict::angles
-	vec3_t  vecOldAngles;
-	float   outStepHeight;        // how much you climbed this move
-	vec3_t  outWishVel;           // This is where you tried
-	vec3_t  outJumpVel;           // This is your jump velocity
-	vec3_t  vecConstraintCenter;
-	float   flConstraintRadius;
-	float   flConstraintWidth;
-	float   flConstraintSpeedFactor;
-	float   flUnknown[5];
-	vec3_t  vecAbsOrigin;
+	bool    first_run_of_instructions : 1;
+	bool    game_code_moved_player : 1;
+	int     player_handle;
+	int     impulse_command;
+	vec3	view_angles;
+	vec3	abs_view_angles;
+	int     buttons;
+	int     old_buttons;
+	float   fw_move;
+	float   sd_move;
+	float   up_move;
+	float   max_speed;
+	float   client_max_speed;
+	vec3	velocity;
+	vec3	angles;
+	vec3	old_angles;
+	float   step_height;
+	vec3	wish_velocity;
+	vec3	jump_velocity;
+	vec3	constraint_center;
+	float   constraint_radius;
+	float   constraint_width;
+	float   constraint_speed_factor;
+
+private:
+	float   u0[ 5 ];
+
+public:
+	vec3	abs_origin;
 };
 
 class virtual_game_movement {
-public:
-	virtual			~virtual_game_movement(void) { }
 
-	virtual void	          process_movement(player_t* pPlayer, player_move_data* pMove) = 0;
-	virtual void	          Reset(void) = 0;
-	virtual void	          start_track_prediction_errors(player_t* pPlayer) = 0;
-	virtual void	          finish_track_prediction_errors(player_t* pPlayer) = 0;
-	virtual void	          DiffPrint(char const* fmt, ...) = 0;
-	virtual vec3_t const& GetPlayerMins(bool ducked) const = 0;
-	virtual vec3_t const& GetPlayerMaxs(bool ducked) const = 0;
-	virtual vec3_t const& GetPlayerViewOffset(bool ducked) const = 0;
-	virtual bool		        IsMovingPlayerStuck(void) const = 0;
-	virtual player_t* GetMovingPlayer(void) const = 0;
-	virtual void		        UnblockPusher(player_t* pPlayer, player_t* pPusher) = 0;
-	virtual void            SetupMovementBounds(player_move_data* pMove) = 0;
+public:
+	virtual				~virtual_game_movement( void ) {}
+	virtual void			process_movement( player_t *player, player_move_data *move ) = 0;
+	virtual void			reset( void ) = 0;
+	virtual void			start_track_prediction_errors( player_t *player ) = 0;
+	virtual void			finish_track_prediction_errors( player_t *player ) = 0;
+	virtual void			diff_print( char const *fmt, ... ) = 0;
+	virtual vec3 const		&get_player_mins( bool ducked ) const = 0;
+	virtual vec3 const		&get_player_maxs( bool ducked ) const = 0;
+	virtual vec3 const		&get_player_view_offset( bool ducked ) const = 0;
+	virtual bool			is_moving_player_stuck( void ) const = 0;
+	virtual player_t		*get_moving_player( void ) const = 0;
+	virtual void			unblock_posher( player_t *player, player_t *pusher ) = 0;
+	virtual void			setup_movement_bounds( player_move_data *move ) = 0;
 };
 
 class player_game_movement
@@ -89,24 +98,25 @@ public:
 };
 
 class player_prediction {
+
 public:
 	bool in_prediction() {
-		typedef bool(__thiscall * oInPrediction)(void*);
-		return utilities::call_virtual<oInPrediction>(this, 14)(this);
+		typedef bool( __thiscall *o_in_prediction )( void * );
+		return utilities::call_virtual<o_in_prediction>( this, 14 )( this );
 	}
 
-	void run_command(player_t* player, c_usercmd* ucmd, player_move_helper* moveHelper) {
-		typedef void(__thiscall * oRunCommand)(void*, player_t*, c_usercmd*, player_move_helper*);
-		return utilities::call_virtual<oRunCommand>(this, 19)(this, player, ucmd, moveHelper);
+	void run_command( player_t *player, c_usercmd *cmd, player_move_helper *helper ) {
+		typedef void( __thiscall *o_run_command )( void *, player_t *, c_usercmd *, player_move_helper * );
+		return utilities::call_virtual<o_run_command>( this, 19 )( this, player, cmd, helper );
 	}
 
-	void setup_move(player_t* player, c_usercmd* ucmd, player_move_helper* moveHelper, void* pMoveData) {
-		typedef void(__thiscall * oSetupMove)(void*, player_t*, c_usercmd*, player_move_helper*, void*);
-		return utilities::call_virtual<oSetupMove>(this, 20)(this, player, ucmd, moveHelper, pMoveData);
+	void setup_move( player_t *player, c_usercmd *cmd, player_move_helper *helper, void *data ) {
+		typedef void( __thiscall *o_setup_move )( void *, player_t *, c_usercmd *, player_move_helper *, void * );
+		return utilities::call_virtual<o_setup_move>( this, 20 )( this, player, cmd, helper, data );
 	}
 
-	void finish_move(player_t* player, c_usercmd* ucmd, void* pMoveData) {
-		typedef void(__thiscall * oFinishMove)(void*, player_t*, c_usercmd*, void*);
-		return utilities::call_virtual<oFinishMove>(this, 21)(this, player, ucmd, pMoveData);
+	void finish_move( player_t *player, c_usercmd *cmd, void *data ) {
+		typedef void( __thiscall *o_finish_move )( void *, player_t *, c_usercmd *, void * );
+		return utilities::call_virtual<o_finish_move>( this, 21 )( this, player, cmd, data );
 	}
 };
