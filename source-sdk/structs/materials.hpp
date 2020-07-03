fe3dc868a2d923edb9f868b0844e732a58638a8a
sove <13748681+behality@users.noreply.github.com>
2020-07-03 22:30:49 +0300

    Update materials.hpp

--- source-sdk/structs/materials.hpp
+++ source-sdk/structs/materials.hpp
@@ -66,21 +66,20 @@ typedef int material_property_types_t;
 typedef unsigned short model_instance_handle_t;
 using material_handle_t = unsigned short;
 
-
 class i_material {
 public:
 	virtual const char* get_name() const = 0;
-	virtual const char* GetTextureGroupName() const = 0;
-	virtual preview_image_retval_t get_preview_image_properties(int* width, int* height, image_format* imageFormat, bool* isTranslucent) const = 0;
-	virtual preview_image_retval_t get_preview_image(unsigned char* data, int width, int height, image_format imageFormat) const = 0;
+	virtual const char* get_texture_group_name() const = 0;
+	virtual preview_image_retval_t get_preview_image_properties(int* width, int* height, image_format* image_format, bool* is_translucent) const = 0;
+	virtual preview_image_retval_t get_preview_image(unsigned char* data, int width, int height, image_format image_format) const = 0;
 	virtual int get_mapping_width() = 0;
 	virtual int get_mapping_height() = 0;
 	virtual int get_num_animation_frames() = 0;
 	virtual bool in_material_page(void) = 0;
-	virtual void get_material_offset(float* pOffset) = 0;
-	virtual void get_material_scale(float* pScale) = 0;
+	virtual void get_material_offset(float* offset) = 0;
+	virtual void get_material_scale(float* scale) = 0;
 	virtual i_material* get_material_page(void) = 0;
-	virtual i_material_var* find_var(const char* varName, bool* found, bool complain = true) = 0;
+	virtual i_material_var* find_var(const char* var_name, bool* found, bool complain = true) = 0;
 	virtual void increment_reference_count(void) = 0;
 	virtual void decrement_reference_count(void) = 0;
 	inline void add_ref() { increment_reference_count(); }
@@ -95,8 +94,8 @@ public:
 	virtual bool has_proxy(void) const = 0;
 	virtual bool uses_env_cubemap(void) = 0;
 	virtual bool needs_tangent_space(void) = 0;
-	virtual bool needs_power_of_two_frame_buffer_texture(bool bCheckSpecificToThisFrame = true) = 0;
-	virtual bool needs_full_frame_buffer_texture(bool bCheckSpecificToThisFrame = true) = 0;
+	virtual bool needs_power_of_two_frame_buffer_texture(bool check_specific_to_this_frame = true) = 0;
+	virtual bool needs_full_frame_buffer_texture(bool check_specific_to_this_frame = true) = 0;
 	virtual bool needs_software_skinning(void) = 0;
 	virtual void alpha_modulate(float alpha) = 0;
 	virtual void color_modulate(float r, float g, float b) = 0;
@@ -105,7 +104,7 @@ public:
 	virtual void get_reflectivity(vec3_t& reflect) = 0;
 	virtual bool get_property_flag(material_property_types_t  type) = 0;
 	virtual bool is_two_sided() = 0;
-	virtual void set_shader(const char* pShaderName) = 0;
+	virtual void set_shader(const char* shader_name) = 0;
 	virtual int get_num_passes(void) = 0;
 	virtual int get_texture_memory_bytes(void) = 0;
 	virtual void refresh() = 0;
@@ -117,15 +116,15 @@ public:
 	virtual void unused() = 0;
 	virtual float get_alpha_modulation() = 0;
 	virtual void get_color_modulation(float* r, float* g, float* b) = 0;
-	virtual bool is_translucent_under_modulation(float fAlphaModulation = 1.0f) const = 0;
-	virtual i_material_var* find_var_fast(char const* pVarName, unsigned int* pToken) = 0;
-	virtual void set_shader_and_params(c_key_values* pKeyValues) = 0;
+	virtual bool is_translucent_under_modulation(float alpha_modulation = 1.0f) const = 0;
+	virtual i_material_var* find_var_fast(char const* var_name, unsigned int* token) = 0;
+	virtual void set_shader_and_params(c_key_values* key_values) = 0;
 	virtual const char* get_shader_name() const = 0;
 	virtual void delete_if_unreferenced() = 0;
 	virtual bool is_sprite_card() = 0;
-	virtual void call_bind_proxy(void* proxyData) = 0;
+	virtual void call_bind_proxy(void* proxy_data) = 0;
 	virtual void refresh_preserving_material_vars() = 0;
 	virtual bool was_reloaded_from_whitelist() = 0;
-	virtual bool set_temp_excluded(bool bSet, int nExcludedDimensionLimit) = 0;
+	virtual bool set_temp_excluded(bool set, int excluded_dimension_limit) = 0;
 	virtual int get_reference_count() const = 0;
 };
