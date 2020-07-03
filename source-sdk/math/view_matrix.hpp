6e2f2823dafdff593e94fcae3bab53b4465cbe5b
sove <13748681+behality@users.noreply.github.com>
2020-07-03 22:35:01 +0300

    Update view_matrix.hpp

--- source-sdk/math/view_matrix.hpp
+++ source-sdk/math/view_matrix.hpp
@@ -50,24 +50,24 @@ public:
 		return &m[0][0];
 	}
 
-	void		set_left(const vec3_t& vLeft);
-	void		set_up(const vec3_t& vUp);
-	void		set_forward(const vec3_t& vForward);
+	void		set_left(const vec3_t& left);
+	void		set_up(const vec3_t& up);
+	void		set_forward(const vec3_t& forward);
 
-	void		get_basis_vector_3d(vec3_t& vForward, vec3_t& vLeft, vec3_t& vUp) const;
-	void		set_basis_vector_3d(const vec3_t& vForward, const vec3_t& vLeft, const vec3_t& vUp);
+	void		get_basis_vector_3d(vec3_t& forward, vec3_t& left, vec3_t& up) const;
+	void		set_basis_vector_3d(const vec3_t& forward, const vec3_t& left, const vec3_t& up);
 
 	// Get/set the translation.
-	vec3_t& get_translation(vec3_t& vTrans) const;
-	void		set_translation(const vec3_t& vTrans);
+	vec3_t& 	get_translation(vec3_t& trans) const;
+	void		set_translation(const vec3_t& trans);
 
-	void		pre_translate(const vec3_t& vTrans);
-	void		post_translate(const vec3_t& vTrans);
+	void		pre_translate(const vec3_t& tans);
+	void		post_translate(const vec3_t& trans);
 
-	matrix_t& as_matrix();
+	matrix_t& 	as_matrix();
 	const matrix_t& as_matrix() const;
 	void		copy_from_matrix(const matrix_t& m3x4);
-	void		set_matrix(matrix_t& matrix3x4) const;
+	void		set_matrix(matrix_t& m3x4) const;
 
 	bool		operator==(const view_matrix_t& src) const;
 	bool		operator!=(const view_matrix_t& src) const { return !(*this == src); }
@@ -81,41 +81,29 @@ public:
 	// Matrix->vec3_t operations.
 public:
 	// Multiply by a 3D vec3_t (same as operator*).
-	void		vector_3d_multiply(const vec3_t & vIn, vec3_t & vOut) const;
-
-	// Multiply by a 4D vec3_t.
-	//void		V4Mul( const vec3_t4D &vIn, vec3_t4D &vOut ) const;
+	void		vector_3d_multiply(const vec3_t & in, vec3_t & out) const;
 
 	// Applies the rotation (ignores translation in the matrix). (This just calls VMul3x3).
-	vec3_t		apply_rotation(const vec3_t & vVec) const;
+	vec3_t		apply_rotation(const vec3_t & vec) const;
 
 	// Multiply by a vec3_t (divides by w, assumes input w is 1).
-	vec3_t		operator*(const vec3_t & vVec) const;
+	vec3_t		operator*(const vec3_t & vec) const;
 
 	// Multiply by the upper 3x3 part of the matrix (ie: only apply rotation).
-	vec3_t		vector_3d_multiply(const vec3_t & vVec) const;
+	vec3_t		vector_3d_multiply(const vec3_t & vec) const;
 
 	// Apply the inverse (transposed) rotation (only works on pure rotation matrix)
-	vec3_t		vector_3d_transpose_rotation(const vec3_t & vVec) const;
+	vec3_t		vector_3d_transpose_rotation(const vec3_t & vec) const;
 
 	// Multiply by the upper 3 rows.
-	vec3_t		vector_3d_multiply_upper(const vec3_t & vVec) const;
+	vec3_t		vector_3d_multiply_upper(const vec3_t & vec) const;
 
 	// Apply the inverse (transposed) transformation (only works on pure rotation/translation)
-	vec3_t		vector_3d_transpose(const vec3_t & vVec) const;
-
-	// Matrix->plane operations.
-	//public:
-	// Transform the plane. The matrix can only contain translation and rotation.
-	//void		TransformPlane( const VPlane &inPlane, VPlane &outPlane ) const;
-
-	// Just calls TransformPlane and returns the result.
-	//VPlane		operator*(const VPlane &thePlane) const;
-
-	// Matrix->matrix operations.
+	vec3_t		vector_3d_transpose(const vec3_t & vec) const;
+	
 public:
 
-	view_matrix_t& operator=(const view_matrix_t & mOther);
+	view_matrix_t& operator=(const view_matrix_t & other);
 
 	// Multiply two matrices (out = this * vm).
 	void		MatrixMul(const view_matrix_t & vm, view_matrix_t & out) const;
@@ -124,7 +112,7 @@ public:
 	const view_matrix_t& operator+=(const view_matrix_t & other);
 
 	// Just calls MatrixMul and returns the result.
-	view_matrix_t		operator*(const view_matrix_t & mOther) const;
+	view_matrix_t		operator*(const view_matrix_t & other) const;
 
 	// Add/Subtract two matrices.
 	view_matrix_t		operator+(const view_matrix_t & other) const;
@@ -141,10 +129,10 @@ public:
 	bool		is_identity() const;
 
 	// Setup a matrix for origin and angles.
-	void		setup_maitrx_orginal_angles(const vec3_t & origin, const vec3_t & vAngles);
+	void		setup_maitrx_orginal_angles(const vec3_t & origin, const vec3_t & angles);
 
 	// Does a fast inverse, assuming the matrix only contains translation and rotation.
-	void		inverse_tr(view_matrix_t & mRet) const;
+	void		inverse_tr(view_matrix_t & ret) const;
 
 	// Usually used for debug checks. Returns true if the upper 3x3 contains
 	// unit vec3_ts and they are all orthogonal.
@@ -157,7 +145,7 @@ public:
 	vec3_t		get_scale() const;
 
 	// (Fast) multiply by a scaling matrix setup from vScale.
-	view_matrix_t		scale(const vec3_t & vScale);
+	view_matrix_t		scale(const vec3_t & scale);
 
 	// Normalize the basis vec3_ts.
 	view_matrix_t		normalize_basis_vector_3d() const;
