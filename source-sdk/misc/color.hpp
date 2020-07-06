b22d65b3c5c62695217c0fb1d64eed8049db6ab6
Marius <53657322+bruhmoment21@users.noreply.github.com>
2020-07-06 21:56:12 +0300

    Update color.hpp

--- source-sdk/misc/color.hpp
+++ source-sdk/misc/color.hpp
@@ -4,29 +4,32 @@
 
 struct color {
 	int a, r, g, b;
-	color() = default;
-	color(int r, int g, int b, int a = 255) {
+	color( ) = default;
+	color( const int r, const int g, const int b, const int a = 255 ) {
 		this->r = r;
 		this->g = g;
 		this->b = b;
 		this->a = a;
 	}
-	color(uint32_t color) {
-		this->a = (color >> 24) & 0xff;
-		this->r = (color >> 16) & 0xff;
-		this->g = (color >> 8) & 0xff;
-		this->b = (color & 0xff);
-	}
-	color from_uint(uint32_t uint) {
-		return color(uint);
-	}
-	D3DCOLOR from_color(color col) {
-		return D3DCOLOR_ARGB(col.a, col.r, col.g, col.b);
+
+	explicit color( const uint32_t color ) {
+		this->a = ( color >> 24 ) & 0xff;
+		this->r = ( color >> 16 ) & 0xff;
+		this->g = ( color >> 8 ) & 0xff;
+		this->b = ( color & 0xff );
 	}
 
-	static color black(int a = 255) { return { 0, 0, 0, a }; }
-	static color white(int a = 255) { return { 255, 255, 255, a }; }
-	static color red(int   a = 255) { return { 255, 0, 0, a }; }
-	static color green(int a = 255) { return { 0, 255, 0, a }; }
-	static color blue(int  a = 255) { return { 0, 0, 255, a }; }
+	static color from_uint( const uint32_t uint ) {
+		return color( uint );
+	}
+
+	static D3DCOLOR from_color( const color col ) {
+		return D3DCOLOR_ARGB( col.a, col.r, col.g, col.b );
+	}
+
+	static color black( const int a = 255 ) { return { 0, 0, 0, a }; }
+	static color white( const int a = 255 ) { return { 255, 255, 255, a }; }
+	static color red( const int   a = 255 ) { return { 255, 0, 0, a }; }
+	static color green( const int a = 255 ) { return { 0, 255, 0, a }; }
+	static color blue( const int  a = 255 ) { return { 0, 0, 255, a }; }
 };
