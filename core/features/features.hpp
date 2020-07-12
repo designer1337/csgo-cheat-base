5d53cc96d49ff49f1fc2a79171472d0777ca8eec
sove <13748681+sowwe@users.noreply.github.com>
2020-07-12 18:38:44 +0300

    Update features.hpp

--- core/features/features.hpp
+++ core/features/features.hpp
@@ -3,7 +3,8 @@
 #include "../menu/variables.hpp"
 
 namespace misc {
-	namespace movement {
-		void bunny_hop(c_usercmd* cmd);
-	};
-}
+void on_create_move();
+namespace movement {
+void bunny_hop();
+} // namespace movement
+} // namespace misc
